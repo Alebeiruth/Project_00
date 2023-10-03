485e048308ffc36352a955485a78660bcdcb037d
Alexandre <alexandrebeyruth@gmail.com>
2023-10-03 17:54:17 -0300

    Adicionar arquivos de projeto.

---
+++ Main.cpp
@@ -0,0 +1,18 @@
+// Fun Fact: This first time with me and C++.
+
+#include <iostream>
+
+
+int main() {
+
+	// Your program lives here:
+
+	std::cout << " SSS    L     \n";
+	std::cout << "S   S   L     \n";
+	std::cout << "S       L     \n";
+	std::cout << " SSS    L     \n";
+	std::cout << "    S   L     \n";
+	std::cout << "S   S   L     \n";
+	std::cout << " SSS    LLLLL \n";
+
+}
